c39adea4f7053b07f06d4e61b2ee21d96b02a54c
moisesfdasilva <msesfernandes@gmail.com>
2023-07-16 18:09:52 -0300

    feat: integrada a requisição à api POST /user/login na página login, com o retorno de usuário não encontrado implementado.

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,37 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../services/api';
 
 function Login() {
+  const [inputLogin, setInputLogin] = useState({
+    username: '',
+    password: '',
+    userNotFound: false,
+  });
+
+  const history = useHistory();
+
+  const inputLoginChange = ({ target }) => {
+    const { name, value } = target;
+    setInputLogin((prevState) => ({
+      ...prevState,
+      userNotFound: false,
+      [name]: value,
+    }));
+  };
+
+  const goToRegisterPage = () => {
+    history.push('/register');
+  }
+
+  const login = async () => {
+    const { username, password } = inputLogin;
+
+    try {
+      const { data } = await api.post('/user/login', { username, password });
+      const { user } = data;
+      console.log(user);
+    } catch {
+      setInputLogin((prevState) => ({ ...prevState, userNotFound: true }));
+    }
+    
+  }
+
+  //useEffect(() => checkToken(), []);
+
   return (
-    <div>
+    <main>
       <form>
         <label htmlFor="username">
           Username:
           <input
+            value={ inputLogin.username }
             type="text"
             name="username"
             placeholder="username"
+            onChange={ inputLoginChange }
           />
         </label>
         <label htmlFor="password">
           Password:
           <input
+            value={ inputLogin.password }
             type="password"
             name="password"
             placeholder="password"
+            onChange={ inputLoginChange }
           />
         </label>
-        <button
-          type="button"
-        >
+        <button type="button" onClick={ login }>
           Login
         </button>
-        <button
-          type="button"
-        >
+        <button type="button" onClick={ goToRegisterPage }>
           Create Account
         </button>
       </form>
-    </div>
+      <div>
+        { inputLogin.userNotFound && <p>Incorrect username or password.</p> }
+      </div>
+    </main>
   );
 }
 
