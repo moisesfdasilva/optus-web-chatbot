12516e9916d276a27b53c7ddf422f1338654dcbf
moisesfdasilva <msesfernandes@gmail.com>
2023-07-17 14:23:38 -0300

    feat: integrada a requisição GET /chat/user/:id/all na página Transition, sendo desenvolvida a exportação das conversas em CSV.

--- frontend/src/pages/transition.jsx
+++ frontend/src/pages/transition.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../services/api';
 
 function Transition() {
+  const [previousMsg, setPreviousMsg] = useState({
+    messages: [],
+  });
+
+  useEffect(() => {
+    const getAllMessages = async () => {
+      const { data } = await api.get('/chat/user/1/all');
+      console.log(data);
+      setPreviousMsg({ messages: data });
+    };
+    getAllMessages();
+  }, []);
+
+  const toCsv = (msgs) => {
+    let sheet = 'date, user, message';
+    msgs.forEach((msg) => {
+      sheet = `${ sheet }\n${ msg.date },${ msg.user },${ msg.message }`;
+    });
+
+    const csvContent = `data:text/csv;charset=utf-8, ${ sheet }`;
+
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link);
+
+    link.click();
+  }
+
   return (
     <>
       <header>
@@ -12,8 +43,17 @@ function Transition() {
           <h2>new</h2>
         </Link>
         <h2>historic:</h2>
-        <p>...</p>
-        <p>...</p>
+        <ul>
+          { (previousMsg.messages.length === 0) && <li>NONE</li> }
+          { (previousMsg.messages.length > 0) && previousMsg.messages.map((msg, index) => (
+            <li key={ index }>
+              <button type="button" onClick={ () => toCsv(msg.messages) }>
+                {`Conversation user #${ msg.userId } -`}
+                {` ${ msg.messages[msg.messages.length - 1].date }`}
+              </button>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
