a24cd345fcb571a6e9d9d44a586c888ab5639872
moisesfdasilva <msesfernandes@gmail.com>
2023-07-19 20:00:54 -0300

    refactor: criadas as variáveis de ambiente de acesso a api em frontend/src/services/api.js.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const HOST = process.env.REACT_HOST || 'localhost:3001';
+const PROTOCOL = process.env.REACT_PROTOCOL || 'http';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: `${ HOST }://${ PROTOCOL }`,
 });
 
 export default api;
