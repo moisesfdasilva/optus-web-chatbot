8fcb5559dd57bf0943e9aadcab23ce2022137391
moisesfdasilva <msesfernandes@gmail.com>
2023-07-19 21:09:02 -0300

    refactor: refatorada a requisição da api adicionando timeout 10000 e headers "Accept: application/json" e "Content-Type: application/json".

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,12 @@ const HOST = process.env.REACT_HOST || 'localhost:3001';
 const PROTOCOL = process.env.REACT_PROTOCOL || 'http';
 
 const api = axios.create({
-  baseURL: `${ HOST }://${ PROTOCOL }`,
+  baseURL: `${PROTOCOL}://${HOST}`,
+  timeout: 10000,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
